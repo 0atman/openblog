a6a802411a8c1422a5847a0d44e7fe30a371a815
Tristram Oaten <tristram@oaten.name>
2020-05-20 16:06:01 +0100

    initial version

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 build:
-	nimble build --passL: -static -d:release openblog.nim
+	nimble build --passL: -static -d:release -d:ssl openblog.nim
+
+run: build
+	./openblog
 
 heroku:
 	git push heroku master
 	heroku logs --tail
+
+watch:
+	watchmedo shell-command --drop --ignore-directories --patterns="*.nim" --ignore-patterns="*#*" --recursive --command='clear && make --no-print-directory build && echo "✔️ Build succeeded!"' .
--- openblog.nim
+++ openblog.nim
@@ -1,13 +1,72 @@
-import jester, asyncdispatch, os, strutils
-import htmlgen as h
+import jester
+import asyncdispatch
+import os
+import strutils
+import htmlgen
+import json
+import strformat
+import FeedNim
+import tables
 
+
+const jsonlist = staticRead "list.json"
+let bloglist = parseJson(jsonlist)["blogs"]
+
+var tags = initTable[string, seq[JsonNode]]()
+
+echo "checking blog list..."
+for blog in bloglist:
+  discard getRSS blog["rss"].getStr
+
+  for rawTag in blog["tags"]:
+    let tag = rawTag.getStr.toLowerAscii.replace(" ", "-")
+    if tags.contains(tag):
+      tags[tag].add blog
+    else:
+      tags[tag] = @[blog]
+
+echo fmt"...{bloglist.len} blogs loaded!"
 var settings = newSettings()
 
 if existsEnv("PORT"):
   settings.port = Port(parseInt(getEnv("PORT")))
 
+func index(tags: Table[string, seq[JsonNode]]): string =
+  var tagLinks: seq[string]
+  for tag in tags.keys:
+    tagLinks.add a(href="/tag/" & tag, tag)
+
+  html(
+    head(link(rel="stylesheet", href="https://newcss.net/new.min.css")),
+    body(
+      header(h1("Open Blog Directory")),
+      tagLinks.join("\n")))
+
+var titles: seq[string]
+for blog in bloglist:
+  titles.add p(blog["name"].getStr)
+
+
+func tagPage(tagname: string, tags: Table[string, seq[JsonNode]]): string =
+  if not tags.contains(tagname):
+    return "no blogs with that tag"
+  var blogList: seq[string]
+  for blog in tags[tagname]:
+    blogList.add li(blog["name"].getStr)
+  html(
+    head(link(rel="stylesheet", href="https://newcss.net/new.min.css")),
+    body(
+      header(h1(tagname)),
+      blogList.join "\n",
+  ))
+
+
 routes:
   get "/":
-    resp h.p("Hello from Openblog")
+    resp index(tags)
+  get "/tag/@tagname":
+    resp tagPage(@"tagname", tags)
+  get "/list.json":
+    resp bloglist
 
 runForever()
